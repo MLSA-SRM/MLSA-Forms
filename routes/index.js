4cd893a46f3579b89650057260b2158f1b08761b
Ayush Kharkia <ayushkharkia3@gmail.com>
2020-09-15 21:43:50 +0530

    logout and redirection

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,9 @@ const router = express.Router();
 const adminRoutes = require('./user/index.admin.routes');
 const userPublicFormRoutes = require('./public/form.routes');
 
+router.get('/', (req, res, next) => {
+    res.redirect('/user/signin');
+})
 
 router.use('/forms', userPublicFormRoutes);
 
